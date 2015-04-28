3dffd4264939a919657a8c1007c431385ebb66fa
Tomas Romero <canotto90@gmail.com>
2015-04-27 23:23:01 -0300

    Add Swal Forms as a submodule

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'canotto90:swal-forms',
   summary: 'Swal-Forms wrapped for meteor',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/taromero/meteor-swal-forms'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0.1');
   api.use('kevohagan:sweetalert@0.5.0')
   api.addFiles([
-    'swal-forms.css',
-    'swal-forms.js'
+    'swal-forms/swal-forms.css',
+    'swal-forms/swal-forms.js'
   ], 'client');
 });
