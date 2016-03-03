06247063684b6d4087ca4e4e698cfd194dc4507f
Tomas Romero <canotto90@gmail.com>
2016-03-03 10:59:35 -0300

    Upgrade kevohagan:sweetalert to 1.0.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'canotto90:swal-forms',
   summary: 'Swal-Forms wrapped for meteor',
-  version: '0.2.0',
+  version: '0.3.0',
   git: 'https://github.com/taromero/meteor-swal-forms'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.1');
-  api.use('kevohagan:sweetalert@0.5.0')
+  api.use('kevohagan:sweetalert@1.0.0')
   api.addFiles([
     'swal-forms/swal-forms.css',
     'swal-forms/swal-forms.js'
